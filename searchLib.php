96c0757549e0f52dc9a6c9432ca288e6a88b9bdf
khellytaguinod <khellytaguinod@gmail.com>
2018-05-04 01:10:32 +0800

    updated searchLib.php

--- searchLib.php
+++ searchLib.php
@@ -3,7 +3,6 @@
 
 $email = $_SESSION["email"];
 
-
 $m = new MongoClient();
 $db = $m->selectDB('blogProj');
 $collection = new MongoCollection($db, 'users');
@@ -13,21 +12,8 @@ $userQuery = array('Email Address' => $email);
 
 $cursor = $collection->find($userQuery);
 
-// $test = $cursor['_id'];
 foreach ($cursor as $doc) {
-    // var_dump($doc);
-        // var_dump($doc['_id']);
-
-$userId = $doc['_id'];
-        
+    $userId = $doc['_id'];
 }
-
-// search for produce that is sweet. Taste is a child of Details. 
-// $sweetQuery = array('Details.Taste' => 'Sweet');
-// echo "Sweet\n";
-// $cursor = $collection->find($sweetQuery);
-// foreach ($cursor as $doc) {
-//     var_dump($doc);
-// }
 
 ?>
